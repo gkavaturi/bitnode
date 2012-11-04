919710665c7d76b294463275b7c2f2afc8c0f8e8
gkavaturi <gkavaturi@forio.com>
2012-11-03 19:40:37 -0700

    reorganized files and some bug fixes

--- server/conc.js
+++ server/conc.js
@@ -1,28 +1,25 @@
 var fs=require('fs');
-var DIR='chunks/';
+var DIR='../chunks/';
 
 	
 var files=fs.readdirSync(DIR);
 var filename='bach.mp3';
 		
-files.forEach(function(singlebit){
+files.forEach(function(singlebit,err){
+	if (err){
+		console.log(err);
+		console.log(singlebit);
+	}
 	//console.log(files);
-	var bit=fs.createReadStream(DIR+singlebit,{ 
-	  flags: 'r+',
-	  encoding: null,
-	  fd: null,
-	  mode: 0666,
-	  bufferSize: 64 * 1024
-	});
+	var bit=fs.createReadStream(DIR+singlebit);
 	var resultfile=fs.createWriteStream(filename,{
-		flags:'r+',
+		flags:'a+',
 		encoding:null,
 		mode:0666,
-		bufferSize: 64 * 1024
+		bufferSize: 1024 * 1024
 	});
 	bit.on('data',function(bitdata){ 
 			resultfile.write(bitdata); 
-			console.log(bitdata);
 		}).on('end',function(){
 			resultfile.end();
 		});

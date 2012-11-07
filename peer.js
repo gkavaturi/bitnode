b04bde09c0e05e75c39b852ee60156889324966e
gkavaturi <gkavaturi@forio.com>
2012-11-06 18:43:20 -0800

    fixed file read errors

--- peer.js
+++ peer.js
@@ -1,7 +1,10 @@
 var http=require('http');
 var fs=require('fs');
-var SERVER_PORT=2011;
-var DIR='/test/peer1/';
+
+
+var SERVER_PORT=(process.argv.length>2 && process.argv[2].split(":")[0]=="port" && !isNaN(process.argv[2].split(":")[1]))?process.argv[2].split(":")[1]:2012;
+
+var DIR=(process.argv.length>3 && process.argv[3].split(":")[0]=="folder" && isNaN(process.argv[3].split(":")[1]))?process.argv[3].split(":")[1]:'./test/peer/';
 
 	
 var ReadTorrent={
@@ -31,7 +34,7 @@ var ReadTorrent={
 
 var getData=function(bitname,options){
 	http.get(options,function(res){
-		var filename=__dirname+DIR+bitname,
+		var filename=DIR+bitname,
 		 	bitfile=fs.createWriteStream(filename);
 		res.on('data',function(data){
 			console.log('STATUS: ' + res.statusCode+'\n');
@@ -49,18 +52,40 @@ var downloadFile=function(filename){
 	var info=ReadTorrent.getPeerInfo(filename);
 	info.forEach(function(data){
 		data.pie.forEach(function(bits){
-		for (var i=0;i<bits.length;i++){
-			console.log(bits[i]+' is present in '+data.loc+':'+data.port);
-			var options = {
-			  host: data.loc,
-			  port: data.port,
-			  path: '/?bitname='+bits[i],
-			  method: 'GET'
-			};
-			getData(bits[i],options);
-		}
+			console.log(bits+' is present in '+data.loc+':'+data.port);	
+			for (var i=0;i<bits.length;i++){
+				var options = {
+				  host: data.loc,
+				  port: data.port,
+				  path: '/?bitname='+bits[i],
+				  method: 'GET'
+				};
+				getData(bits[i],options);
+			}
 		});
 	});
+	return true;
+}
+
+var concanateFile=function(filename){
+	var files=fs.readdirSync(DIR);
+	files.forEach(function(singlebit){
+		//console.log(files);
+		var bit=fs.createReadStream(DIR+singlebit);
+		var resultfile=fs.createWriteStream(filename,{
+			flags:'a+',
+			encoding:null,
+			mode:0666,
+			bufferSize: 64 * 1024
+		});
+		bit.on('data',function(bitdata){ 
+				resultfile.write(bitdata); 
+			}).on('end',function(){
+				resultfile.end();
+			});
+		});
+	console.log('file complete');
+
 }
 
 
@@ -70,13 +95,22 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data',function(text){
 text=text.toString().trim();
 var instr=text.split(' ');
-process.stdout.write('Opening '+instr[1]+'\n');
 if(text=='quit'){
 	process.stdout.write('now exiting\n');
 	process.exit();
 }
 if (instr[0]=='start' && instr[1]){
-	downloadFile(instr[1]);
+	process.stdout.write('Opening '+instr[1]+'\n');
+	//console.log(instr[1]);
+	//downloadFile(instr[1]);
+	
+	if (downloadFile(instr[1])==true){
+		concanateFile('bach.mp3');
+		process.stdout.write('Download complete');
+	}else{
+		process.stdout.write('Oops! Something went wrong. Please try again or send an email to support along with the link to the file you were trying to download\n');
+		process.exit();
+	}
 }else{
 	process.stdout.write('undefined command')
 }	
@@ -84,7 +118,7 @@ if (instr[0]=='start' && instr[1]){
 
 
 http.createServer(function(req,res){
-	var filename=__dirname+DIR+req.url.split("=")[1],
+	var filename=__dirname+'/'+DIR+req.url.split("=")[1],
 		upstream=fs.createReadStream(filename);
 	console.log('opening filename '+filename);
 	console.log(upstream);	
@@ -97,4 +131,4 @@ http.createServer(function(req,res){
 	});
 }).listen(SERVER_PORT);
 
-console.log('listening on '+SERVER_PORT);
+console.log('listening on '+SERVER_PORT+' download dir: '+DIR);

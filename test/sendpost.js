47fbd16fd0a3cc54b4d39557896f8fc9684ae74d
gkavaturi <gkavaturi@forio.com>
2012-11-19 16:02:24 -0800

    added progress bar tracking

--- test/sendpost.js
+++ test/sendpost.js
@@ -1,18 +1,45 @@
-var http=require('http');
+var http=require('http'),
+    crypto=require('crypto'),
+    os=require('os'),
+    querystring=require('querystring');
 
-var options={
-	host:'localhost',
-	port:3000,
-	path:'/upload',
-	method:'POST'
-};
 
+var cleanString=function(str){
+	var cleanstring="";
+	for(var i=0;i<str.length;i++){
+		if (!str.charAt(i).match(/\s+/gi) && str.charCodeAt(i)!=127){
+			cleanstring=cleanstring+str.charAt(i);
+		}
+	}
+	return cleanstring;
+}
+
+var getUniqueId=function(){
+	return crypto.createHash('md5').update(os.hostname()).digest('binary');
+}
+var uniqueid=cleanString(getUniqueId());
+var post_data = querystring.stringify({
+        'uniqueid' : uniqueid
+  });
+  
+  var options={
+  	host:'localhost',
+  	port:3000,
+  	path:'/remember',
+  	method:'POST',
+  	headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Content-Length': post_data.length
+          }
+  };
+ 
+ 
 var req=http.request(options,function(res){
 	console.log('Status: '+res.statusCode);
 	console.log('Headers: '+JSON.stringify(res.headers));
 	res.setEncoding('utf8');
 	res.on('data',function(chunk){
-		console.log('Body: '+chunk);
+		console.log(chunk);
 	});
 });
 
@@ -20,9 +47,14 @@ req.on('error',function(e){
 	console.log('problem with request: '+e.message);
 });
 
+/*
 var data={};
 data.uniqueid="ì:GQî\u000bôÔc(/:BÐn";
 data.bitname="piece19.data";
 data.filename="bach_arai.json";
 req.write(JSON.stringify(data));
+*/
+
+console.log(post_data);
+req.write(post_data);
 req.end();
